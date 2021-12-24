715ddee711cbece9c1d620ee6b0773110cb3ed1e
Bruno Domingues <brunood07@gmail.com>
2021-12-24 11:45:36 -0300

    CreateClient criado

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,11 @@
 import express from "express";
 
+import { router } from "./routes";
+
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({
-    message: "Hello World!"
-  });
-});
+app.use(router);
 
 export { app };
